4d6ba5a035b32c04d1273a75c2ad12f079733944
chenwenhang <cwh736@qq.com>
2019-11-08 17:35:31 +0800

    feat(): update dependencies and update product setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: Chen Wenhang
+ * @Date: 2019-10-19 22:00:31
+ * @LastEditTime: 2019-11-08 16:35:19
+ * @Description: 
+ * @Github: https://github.com/chenwenhang
+ */
 export const environment = {
   SERVER_URL: `http://localhost:3000`,
   production: true,
