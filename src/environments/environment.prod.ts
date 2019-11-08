3eb9e933da839982542f147618ef74317f1c76ff
chenwenhang <cwh736@qq.com>
2019-11-08 22:17:36 +0800

    feat($dist & $environment): update environment setting and recreate dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: Chen Wenhang
  * @Date: 2019-10-19 22:00:31
- * @LastEditTime: 2019-11-08 16:35:19
+ * @LastEditTime: 2019-11-08 21:50:24
  * @Description: 
  * @Github: https://github.com/chenwenhang
  */
 export const environment = {
-  SERVER_URL: `http://localhost:3000`,
+  SERVER_URL: `http://geekdata.top:3000`,
   production: true,
   useHash: true,
   hmr: false,
